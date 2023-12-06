61990e64de0898010960b91469971138415449c4
vivekvag <vivekvag281@gmail.com>
2023-12-06 14:09:05 +0530

    Successfully done with the stroke color upon leafnode click

--- src/App.js
+++ src/App.js
@@ -1,11 +1,51 @@
-import "./App.css"
-import Tree from "./Tree";
+// App.js
+import React from 'react';
+import HierarchyChart from './Tree';
+import './index.css'
 
-export default function App() {
+const treeData = {
+  name: 'John Doe',
+  children: [
+    {
+      name: 'iPhone',
+      children: [
+        {
+          name: '4',
+        },
+        {
+          name: '5',
+        },
+      ],
+    },
+    {
+      name: 'Mac',
+      children: [
+        {
+          name: '1',
+        },
+        {
+          name: '2',
+        },
+      ],
+    },
+    {
+      name: 'Windows',
+      children: [
+        {
+          name: '3',
+        },
+      ],
+    },
+  ],
+};
+
+const App = () => {
+
   return (
-    <div className="App">
-      <h1>D3 Tree example</h1>
-      <Tree />
-    </div>
+      <HierarchyChart
+        data={treeData}
+      />
   );
-}
+};
+
+export default App;
--- src/Tree.js
+++ src/Tree.js
@@ -1,146 +1,98 @@
-import React from "react";
-import Tree from "react-d3-tree";
-import { confirmAlert } from "react-confirm-alert";
-import "react-confirm-alert/src/react-confirm-alert.css";
+import React, { useRef, useEffect } from 'react';
+import { confirmAlert } from 'react-confirm-alert'; 
+import 'react-confirm-alert/src/react-confirm-alert.css'; 
+import * as d3 from 'd3';
 
+const HierarchyChart = ({ data  }) => {
 
-
-const debugData = [
-  {
-    name: "Grapql API",
-    children: [
-      {
-        name: "Query",
-        children: [
-          {
-            name: "1"
-          },
-        ]
-      },
-      {
-        name: "Query",
-        children: [
-          {
-            name: "1"
-          },
-          {
-            name: "2"
-          },
-        ]
-      },
-      {
-        name: "Query",
-        children: [
-          {
-            name: "1"
-          },
-          {
-            name: "2"
-          },
-          {
-            name: "3"
-          },
-        ]
-      },
-      {
-        name: "Query",
-        children: [
-          {
-            name: "2"
-          },
-          {
-            name: "3"
-          },
-        ]
-      }
-    ]
-  }
-];
-
-const containerStyles = {
-  width: "100%",
-  height: "100vh",
-};
-
-const svgSquare = {
-  shape: "rect",
-  shapeProps: {
-    width: 5,
-    height: 5,
-    x: -5,
-    y: -5,
-  },
-};
-
-export default class CenteredTree extends React.PureComponent {
-
-  state = {
-    selectedNode: null,
-  };
-
-  componentDidMount() {
-    const dimensions = this.treeContainer.getBoundingClientRect();
-    this.setState({
-      translate: {
-        x: dimensions.width / 10,
-        y: dimensions.width / 3,
-      },
+  const submit = (d) => {
+    confirmAlert({
+      title: 'Confirm to submit',
+      message: 'Are you sure to do this.',
+      buttons: [
+        {
+          label: 'Yes',
+          onClick: () => alert('Click Yes')
+        },
+        {
+          label: 'Terminate',
+          onClick: () => {
+            updatePathColor(d);
+            console.log(d)
+          }
+        }
+      ]
     });
-  }
-
-  handleNodeClick = (nodeData) => {
-    // Check if the clicked node is a leaf node
-    if (!nodeData.children || nodeData.children.length === 0) {
-      // Call your function here
-      console.log("Leaf node clicked:", nodeData.name);
-
-      confirmAlert({
-        title: "Confirm to submit",
-        message: "Are you sure to do this.",
-        buttons: [
-          {
-            label: "Yes",
-            onClick: () => alert("Click Yes"),
-          },
-          {
-            label: "No",
-            onClick: () => {
-              this.setState({ selectedNode: nodeData });
-              console.log()
-            },
-          },
-        ],
-      });
-    }
   };
 
-  render() {
-    return (
-      <div style={containerStyles} ref={(tc) => (this.treeContainer = tc)}>
-        <Tree
-          data={debugData}
-          translate={this.state.translate}
-          orientation={"vertical"}
-          nodeSvgShape={(nodeData) => {
-            const isRedStroke =
-              this.state.selectedNode &&
-              this.state.selectedNode.name === nodeData.name;
+  const updatePathColor = (d) => {
+    const svg = d3.select(svgRef.current);
 
-            return {
-              shape: "rect",
-              shapeProps: {
-                width: 5,
-                height: 5,
-                x: -5,
-                y: -5,
-                stroke: isRedStroke ? "red" : "black",
-              },
-            };
-          }}
-          circleRadius={5}
-          onNodeClick={this.handleNodeClick}
-        />
-      </div>
-    );
-  }
-}
+    // Change color for the selected path and its parent
+    svg
+      .selectAll('.link')
+      .filter((link) => link.target === d || link.source === d)
+      .attr('stroke', 'skyblue');
+  };
+
+  const svgRef = useRef(null);
+
+  useEffect(() => {
+    if (data) {
+      // Create a hierarchy layout
+      const hierarchy = d3.hierarchy(data);
+
+      // Set up the tree layout
+      const treeLayout = d3.tree().size([500, 300]);
+
+      // Assign positions to each node in the hierarchy
+      treeLayout(hierarchy);
+
+      // Select the SVG container
+      const svg = d3.select(svgRef.current);
+
+      // Create links
+      const links = svg
+        .selectAll('.link')
+        .data(hierarchy.links())
+        .enter()
+        .append('path')
+        .attr('class', 'link')
+        .attr('d', d3.linkHorizontal().x((d) => d.y).y((d) => d.x))
+        .attr('stroke', '#ccc');
+
+      // Create nodes
+      const nodes = svg
+        .selectAll('.node')
+        .data(hierarchy.descendants())
+        .enter()
+        .append('g')
+        .attr('class', 'node')
+        .attr('transform', (d) => `translate(${d.y},${d.x})`)
+        .on('click', (event, d) => {
+          // Check if the node is a leaf node (has no children)
+          if (!d.children && !d._children) {
+            submit(d); // Call the submit function for leaf nodes
+          }
+        });
+
+      // Add circles to represent nodes
+      nodes.append('circle').attr('r', 5);
+
+      // Add text labels
+      nodes
+        .append('text')
+        .attr('dy', '.35em')
+        .attr('x', (d) => (d.children ? -8 : 8))
+        .style('text-anchor', (d) => (d.children ? 'end' : 'start'))
+        .text((d) => d.data.name);
+    }
+  }, [data]);
+
+  return (
+    <svg ref={svgRef} width={600} height={600}>
+    </svg>
+  );
+};
+
+export default HierarchyChart;
