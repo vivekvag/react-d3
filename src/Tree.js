6e86906e0bbc83bd3d175f879355aaf4ea71484a
vivekvag <vivekvag281@gmail.com>
2023-12-06 16:10:32 +0530

    optimized the code

--- src/Tree.js
+++ src/Tree.js
@@ -3,31 +3,35 @@ import { confirmAlert } from 'react-confirm-alert';
 import 'react-confirm-alert/src/react-confirm-alert.css';
 import * as d3 from 'd3';
 
+const TREE_SIZE = [500, 300];
+const CIRCLE_RADIUS = 5;
+
 const HierarchyChart = ({ data }) => {
-    
   const isNodeTerminated = (d) => {
     const svg = d3.select(svgRef.current);
-  
-    // Get the current color of the selected path
     const currentColor = svg
       .selectAll('.link')
       .filter((link) => link.target === d || link.source === d)
       .attr('stroke');
-  
     return currentColor === 'red';
   };
-  
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+
+  const updatePathColor = (d) => {
+    const svg = d3.select(svgRef.current);
+    const newColor = isNodeTerminated(d) ? '#ccc' : 'red';
+    svg
+      .selectAll('.link')
+      .filter((link) => link.target === d || link.source === d)
+      .attr('stroke', newColor);
+  };
+
   const submit = (d) => {
     const isTerminated = isNodeTerminated(d);
-  
-    const title = isTerminated
-      ? 'Enable the Line'
-      : 'Confirm to terminate';
+    const title = isTerminated ? 'Enable the Line' : 'Confirm to terminate';
     const message = isTerminated
       ? 'Are you sure to undo termination?'
       : 'Are you sure to terminate?';
-  
+
     confirmAlert({
       title,
       message,
@@ -46,102 +50,77 @@ const HierarchyChart = ({ data }) => {
       ],
     });
   };
-  
 
-  const updatePathColor = (d) => {
-    const svg = d3.select(svgRef.current);
-    
-      // Toggle between red and #ccc
-      const newColor =  isNodeTerminated(d)  ? '#ccc' : 'red';
+  const createTree = () => {
+    const hierarchy = d3.hierarchy(data);
+    const treeLayout = d3.tree().size(TREE_SIZE);
+    treeLayout(hierarchy);
+    return hierarchy;
+  };
 
-      // Update the color for the selected path and its parent
-      svg
-        .selectAll('.link')
-        .filter((link) => link.target === d || link.source === d)
-        .attr('stroke', newColor);
+  const createLinks = (svg, hierarchy) => {
+    svg
+      .selectAll('.link')
+      .data(hierarchy.links())
+      .enter()
+      .append('path')
+      .attr('class', 'link')
+      .attr('d', d3.linkHorizontal().x((d) => d.y).y((d) => d.x))
+      .attr('stroke', '#ccc');
+  };
+
+  const createNodes = (svg, hierarchy) => {
+    const nodes = svg
+      .selectAll('.node')
+      .data(hierarchy.descendants())
+      .enter()
+      .append('g')
+      .attr('class', 'node')
+      .attr('transform', (d) => `translate(${d.y},${d.x})`)
+      .on('click', (event, d) => {
+        if (!d.children && !d._children) {
+          submit(d);
+        }
+      });
+
+    nodes.each(function (d) {
+      const node = d3.select(this);
+
+      if (d.data.image) {
+        node
+          .append('image')
+          .attr('xlink:href', d.data.image)
+          .attr('x', -12)
+          .attr('y', 0)
+          .attr('width', 24)
+          .attr('height', 24);
+      } else {
+        node
+          .append('circle')
+          .attr('r', CIRCLE_RADIUS)
+          .attr('x', -12)
+          .attr('y', 0);
+
+        node
+          .append('text')
+          .attr('dy', '.35rem')
+          .attr('x', (d) => (d.children ? -8 : 8))
+          .style('text-anchor', (d) => (d.children ? 'end' : 'start'))
+          .text((d) => d.data.name);
+      }
+    });
   };
 
   const svgRef = useRef(null);
 
   useEffect(() => {
     if (data) {
-      // Create a hierarchy layout
-      const hierarchy = d3.hierarchy(data);
-
-      // Set up the tree layout
-      const treeLayout = d3.tree().size([500, 300]);
-
-      // Assign positions to each node in the hierarchy
-      treeLayout(hierarchy);
-
-      // Select the SVG container
       const svg = d3.select(svgRef.current);
-
-      // Create a zoom behavior
-      const zoom = d3.zoom().on('zoom', (event) => {
-        svg.attr('transform', event.transform);
-      });
-
-      // Apply the zoom behavior to the SVG container
-      svg.call(zoom);
-
-      // Create links
-      svg
-        .selectAll('.link')
-        .data(hierarchy.links())
-        .enter()
-        .append('path')
-        .attr('class', 'link')
-        .attr('d', d3.linkHorizontal().x((d) => d.y).y((d) => d.x))
-        .attr('stroke', '#ccc');
-
-      // Create nodes
-      const nodes = svg
-        .selectAll('.node')
-        .data(hierarchy.descendants())
-        .enter()
-        .append('g')
-        .attr('class', 'node')
-        .attr('transform', (d) => `translate(${d.y},${d.x})`)
-        .on('click', (event, d) => {
-          // Check if the node is a leaf node (has no children)
-          if (!d.children && !d._children) {
-            submit(d); // Call the submit function for leaf nodes
-          }
-        });
-
-      // Add text or image labels
-      nodes.each(function (d) {
-        const node = d3.select(this);
-
-        if (d.data.image) {
-          // If the node has an image property, show the image
-          node
-            .append('image')
-            .attr('xlink:href', d.data.image)
-            .attr('x', -12)
-            .attr('y', 0)
-            .attr('width', 24)
-            .attr('height', 24);
-        } else {
-          // Add circles to represent nodes
-          node
-            .append('circle')
-            .attr('r', 5)
-            .attr('x', -12)
-            .attr('y', 0);
-
-          // If the node doesn't have an image property, show the text
-          node
-            .append('text')
-            .attr('dy', '.35rem')
-            .attr('x', (d) => (d.children ? -8 : 8))
-            .style('text-anchor', (d) => (d.children ? 'end' : 'start'))
-            .text((d) => d.data.name);
-        }
-      });
+      const hierarchy = createTree();
+      createLinks(svg, hierarchy);
+      createNodes(svg, hierarchy);
     }
-  }, [data, submit]);
+  }, [data]);
 
   return <svg ref={svgRef} width={600} height={600}></svg>;
 };
